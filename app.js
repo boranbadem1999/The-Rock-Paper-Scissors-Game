69609718f0e5bf793de06686ed1a7836db171bbb
boranbadem1999 <89799297+boranbadem1999@users.noreply.github.com>
2023-07-31 17:13:38 +0300

    First Update

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const playTheGame = document.querySelector(".play-click");
 const gameButtons = document.querySelectorAll(".game-buttons");
 const gameParagraph = document.querySelector(".game-paragraph");
 const resultDisplay = document.querySelector(".result-display");
+const plyAgain = document.querySelector(".play-again");
 
 let choiceOfUser;
 let choiceOfComputer;
+
+let userScore = 0;
+let computerScore = 0;
+plyAgain.classList.add("display-negative");
 
 const ourResult = () => {
   switch (choiceOfUser + choiceOfComputer) {
@@ -14,11 +19,13 @@ const ourResult = () => {
     case "RockScissors":
     case "PaperRock":
       resultDisplay.innerHTML = "YOU WIN";
+      userScore++;
       break;
     case "PaperScissors":
     case "ScissorsRock":
     case "RockPaper":
       resultDisplay.innerHTML = "YOU LOSE";
+      computerScore++;
       break;
     case "RockRock":
     case "PaperPaper":
@@ -27,24 +34,57 @@ const ourResult = () => {
       break;
   }
 
+  gameH1.innerHTML = `Your Choice : ${choiceOfUser} <br><br> Computer's Choice: ${choiceOfComputer}`;
+  resultDisplay.innerHTML += `<br><br> Your Score: ${userScore} <br> Computer Score: ${computerScore}`;
+
+  if (userScore === 10) {
+    resultDisplay.innerHTML +=
+      "<br><br> Congratulations! You reached 10 points. You WON!";
+    plyAgain.classList.remove("display-negative");
+  } else if (computerScore === 10) {
+    resultDisplay.innerHTML += "<br><br> Computer reached 10 points. You LOST!";
+    plyAgain.classList.remove("display-negative");
+  }
   resultDisplay.classList.remove("display-negative");
+};
+
+const disableButtons = () => {
+  gameButtons.forEach((button) => {
+    button.removeEventListener("click", handleClick);
+    button.classList.add("negative-buttons");
+  });
 };
 
 playTheGame.addEventListener("click", function () {
   playTheGame.classList.add("display-negative");
   gameParagraph.classList.add("display-negative");
+  userScore = 0;
+  computerScore = 0;
 
   const choices = ["Rock", "Paper", "Scissors"];
 
   const handleClick = (e) => {
-    choiceOfUser = e.target.textContent.trim();
-    choiceOfComputer = choices[Math.floor(Math.random() * choices.length)];
-    gameH1.innerHTML = `Our Choice : ${choiceOfUser} <br><br> Computer's Choice: ${choiceOfComputer}`;
-    ourResult();
+    if (userScore < 10 && computerScore < 10) {
+      choiceOfUser = e.target.textContent.trim();
+      choiceOfComputer = choices[Math.floor(Math.random() * choices.length)];
+      ourResult();
+
+      if (userScore === 10 || computerScore === 10) {
+        disableButtons();
+      }
+    }
   };
 
   gameButtons.forEach((button) => {
     button.classList.remove("negative-buttons");
     button.addEventListener("click", handleClick);
   });
+});
+
+plyAgain.addEventListener("click", function () {
+  userScore = 0;
+  computerScore = 0;
+  resultDisplay.innerHTML = " ";
+  gameH1.innerHTML = "";
+  plyAgain.classList.add("display-negative");
 });
